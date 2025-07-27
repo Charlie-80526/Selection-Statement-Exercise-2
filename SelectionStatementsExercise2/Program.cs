c5e4b732d0a165b0659b5f832d61a6d68febbbfe
Luis Yanez <lyanez0210@hotmail.com>
2025-07-27 11:08:53 -0500

    initial commit

--- SelectionStatementsExercise2/Program.cs
+++ SelectionStatementsExercise2/Program.cs
@@ -4,7 +4,35 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            FavSubject();
+        }
+
+        public static void FavSubject()
+        {
+            Console.WriteLine("What is your favorite school subject?");
+            string subject = Console.ReadLine();
+
+            switch (subject.ToLower())
+            {
+                case "math":
+                    Console.WriteLine("I don't prefer math at all");
+                    break;
+                case "science":
+                    Console.WriteLine("Science Rocks");
+                    break;
+                case "languages":
+                    Console.WriteLine("languages are fun");
+                    break;
+                case "coding":
+                    Console.WriteLine("Coding is interesting");
+                    break;
+                case "literature":
+                    Console.WriteLine("Literature is my favorite too");
+                    break;
+                default:
+                    Console.WriteLine("I like that too");
+                    break;
+            }
         }
     }
 }
